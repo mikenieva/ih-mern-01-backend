691fd1053dd7d730b656d9835786b29bad24895f
Mike Nieva <m@mikenieva.com>
2021-12-14 12:19:14 -0600

    Integración de JSONWEBTOKENS

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const jwt		= require("jsonwebtoken")
 
 const User		= require("./../models/User")
 
-
+// CREAR UN USUARIO
 exports.create = async (req, res) => {
 
 	// 1. OBTENER USUARIO, EMAIL Y PASSWORD DEL FORMULARIO (REQ)
@@ -69,5 +69,103 @@ exports.create = async (req, res) => {
 
 	}
 
+
+}
+
+
+// INICIAR SESIÓN
+// AUTENTICAR QUE LA PERSONA PASE SU EMAIL Y CONTRASEÑA. COINCIDAN. Y SE LE ENVÍA UN TOKEN.
+exports.login = async (req, res) => {
+
+	// 1. OBTENER EL EMAIL Y EL PASSWORD DEL FORMULARIO (JSON)
+	const { email, password } = req.body
+
+	try {
+		// 2. ENCONTRAR UN USUARIO EN BASE DE DATOS
+		const foundUser = await User.findOne({ email })
+
+		// 3. VALIDACIÓN - SI NO HUBO UN USUARIO...
+		if(!foundUser) {
+			return res.status(400).json({
+				msg: "El usuario o la contraseña son incorrectos."
+			})
+		}
+
+		// 4. SI TODO OK, EL USUARIO FUE ENCONTRADO, ENTONCES, EVALUAMOS LA CONTRASEÑA.
+		const verifiedPass = await bcryptjs.compare(password, foundUser.password)
+
+		// 5. VALIDACIÓN - SI EL PASSWORD NO COINCIDE...
+		if(!verifiedPass) {
+			return await res.status(400).json({
+				msg: "El usuario o la contraseña no coinciden."
+			})
+		}
+
+		// 6. SI TODO COINCIDE Y ES CORRECTO, GENERAMOS UN JSON WEB TOKEN
+
+		console.log("foundUser:", foundUser)
+
+		// 6A. ESTABLECER UN PAYLOAD (DATOS DEL USUARIO)
+		const payload = {
+			user: {
+				id: foundUser.id
+			}
+		}
+
+		// 6B. FIRMA DEL JWT
+		jwt.sign(
+			payload,
+			process.env.SECRET,
+			{
+				expiresIn: 360000
+			},
+			(error, token) => {
+				if(error) throw error
+
+				res.json({
+					msg: "Inicio de sesión exitoso.",
+					data: token
+				})
+			}
+		)
+		
+		return
+
+
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({
+			msg: "Hubo un problema con la autenticación.",
+			data: error
+		})
+	}
+
+}
+
+// VERIFICAR USUARIO
+// CUANDO ESTAMOS ACCEDIENDO A DIFERENTES RUTAS (GUITARRAS COMO TIENDAS) PREGUNTAR SI EL USUARIO TIENE PERMISOS O NO. ENTONCES, PARA CONFIRMARLO, SE LE PIDE SU TOKEN.
+// UNA RUTA QUE PIDE TOKENS PARA VERIFICAR
+exports.verifyToken = async (req, res) => {
+
+
+	try {
+		
+		// 1. BUSCAR EL ID DEL USUARIO (DEL TOKEN ABIERTO) EN BASE DE DATOS
+
+		const foundUser = await User.findById(req.user.id).select("-password")
+
+		return res.json({
+			msg: "Datos de usuario encontrados.",
+			data: foundUser
+		})
+
+
+	} catch (error) {
+			console.log(error)
+
+			res.status(500).json({
+				msg: "Hubo un error con el usuario"
+			})
+	}
 
 }
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,18 @@ const router	= express.Router()
 
 const userController	= require("./../controllers/userController")
 
+const authorization 	= require("./../middleware/authorization")
+
 // 2. ROUTER
+// CREAR USUARIO
 router.post("/create", userController.create)
+
+// INICIAR SESIÓN DE USUARIO
+router.post("/login", userController.login)
+
+// VERIFICACIÓN DE USUARIO
+router.get("/verifytoken", authorization, userController.verifyToken)
+
 
 // 3. EXPORTACIÓN
 module.exports = router
